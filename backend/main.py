59c12c7ebed0b76f98cb4511ed55c656d7a67109
AbhaySrivastava26 <abhaysrivastava2627@gmail.com>
2026-03-18 08:47:03 +0530

    fix cors for production

--- backend/main.py
+++ backend/main.py
@@ -20,10 +20,7 @@ app = FastAPI(title="HelpIQ API", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000", "http://localhost:5173",
-        "http://127.0.0.1:5173", "http://127.0.0.1:3000",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
